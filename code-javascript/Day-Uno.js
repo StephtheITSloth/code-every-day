028beea48211fc921f5a98ff055e2dc322a24e30
stephane-karim225 <stephan.karim.sk@outlook.com>
2021-04-07 10:58:45 -0400

    Python day uno

---
+++ code-javascript/Day-Uno.js
@@ -0,0 +1,8 @@
+//Coding in Python just to refresh my mind and be use to the syntax
+
+// def isXEven(x) :
+//     if x % 2 == 0:
+//         print('x is even')
+//     else :
+//         print('x is odd')
+//     return ('All done.')
